fa0eaadc9898c503a7beb62c96f34c0eabea791e
Vlad <vlad.lemkov@yandex.ru>
2024-11-25 12:08:32 +0400

    fix object keys

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,6 +1,10 @@
-import { AboutPage } from "@/pages/AboutPage"
-import { MainPage } from "@/pages/MainPage"
-import { RouteProps } from "react-router-dom"
+import { RouteProps } from "react-router-dom";
+
+
+
+import { AboutPage } from "@/pages/AboutPage";
+import { MainPage } from "@/pages/MainPage";
+
 
 export enum AppRoutes {
     MAIN = "main",
@@ -13,12 +17,12 @@ export const RoutePath: Record<AppRoutes, string> = {
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
-    [AppRoutes.MAIN]: {
-        path: RoutePath.main,
-        element: <MainPage />,
-    },
-    [AppRoutes.ABOUT]: {
-        path: RoutePath.about,
-        element: <AboutPage />,
-    }
-}
+	[AppRoutes.MAIN]: {
+		path: RoutePath[AppRoutes.MAIN],
+		element: <MainPage />,
+	},
+	[AppRoutes.ABOUT]: {
+		path: RoutePath[AppRoutes.ABOUT],
+		element: <AboutPage />,
+	},
+};
